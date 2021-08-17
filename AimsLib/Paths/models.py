08fb4e3babe3aae7fa1324920f1cba2d0fe83f69
aimwps <mwpskinner@gmail.com>
2021-08-17 10:02:20 +0100

    Built base models for Paths. Quiz's lectures and there partnering models. Premigration

--- AimsLib/Paths/models.py
+++ AimsLib/Paths/models.py
@@ -4,3 +4,51 @@ from django.urls import reverse
 from datetime import datetime, date
 from django.db.models import Q
 from django.core.validators import MaxValueValidator, MinValueValidator
+from ckeditor.fields import RichTextField
+
+class Quiz(models.Model): # A collection of questions
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = RichTextField(config_name="article_editor", )
+    publish_date = models.DateField(auto_now_add=True)
+    publish_time = models.TimeField(auto_now_add=True)
+    def __str__(self):
+        return f"<Quiz : {self.title}>"
+
+
+class QuizQuestion(models.Model):
+    ANSWER_TYPES = (("multiple-choice", "Multiple choice"),
+                    ("multiple-correct-choice","Multiple correct choices"),
+                    ("text-entry-exact", "Text entry (Exact)"),
+                    ("text-entry-nearest", "Text entry (Close enough)"))
+
+    on_quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    answer_type = models.CharField(max_length=255, choices=ANSWER_TYPES)
+    def __str__(self):
+        return f"<QuizQuestion : {self.answer_type}>"
+
+class QuizAnswer(models.Model):
+    to_question = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    answer_text = models.TextField()
+    def __str__(self):
+        return f"<QuizAnswer>"
+
+class VideoLecture(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateField(auto_now_add=True)
+    publish_time = models.TimeField(auto_now_add=True)
+    video_link = models.URLField(max_length=255)
+    def __str__(self):
+        return f"<VideoLecture : {self.title}>"
+
+class WrittenLecture(models.Model): # A body of text with extra elements e.g. Data Tables, recipes or images
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish_date = models.DateField(auto_now_add=True)
+    publish_time = models.TimeField(auto_now_add=True)
+    body = RichTextField(config_name="article_editor", )
+    def __str__(self):
+        return f"<WrittenLecture : {self.title}>"
--- AimsLib/Paths/views.py
+++ AimsLib/Paths/views.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView, CreateView, View
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-
+from ckeditor.fields import RichTextField
 class PathsHomeView(TemplateView):
     template_name = "paths.html"
