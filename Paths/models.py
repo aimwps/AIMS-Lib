2b56186716aab5923deaf81dc597b6e475750ae8
aimwps <mwpskinner@gmail.com>
2021-09-01 11:09:53 +0100

    migrated questionbankmodel

--- Paths/models.py
+++ Paths/models.py
@@ -35,11 +35,18 @@ class GeneratedQuestionBank(models.Model):
     SOURCE_TYPE = (("video-transcript","video-transcript"),
                             ("written-lecture","written-lecture"),
                             ("uploaded-document", "uploaded-document"))
+    PROOF_OPTIONS =(("perfect", "perfect"),
+                    ("editable", "editable"),
+                    ("incorrect","incorrect"),
+                    ("unknown", "unknown"))
     generated_date = models.DateField(auto_now_add=True)
     generated_time = models.TimeField(auto_now_add=True)
     generated_by = models.ForeignKey(User, on_delete=models.CASCADE)
     source_type = models.CharField(max_length=255, choices=SOURCE_TYPE)
     source_id = models.PositiveIntegerField()
+    question = models.TextField()
+    answer = models.TextField()
+    user_proof = models.CharField(max_length=255, choices=PROOF_OPTIONS)
 
 
 
