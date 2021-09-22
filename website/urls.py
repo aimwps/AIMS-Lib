6f2b4fd5c9ff8ec0bf60bb9dccab10da2f7d4c39
aimwps <mwpskinner@gmail.com>
2021-09-22 10:48:34 +0100

    fixed aimwps bug

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import LoginRegisterRequiredView
+from .views import LoginRegisterRequiredView,  AIMwpSView
 urlpatterns = [
         path('', views.home, name="home"),
         path('login-or-register/', LoginRegisterRequiredView.as_view(), name="login-or-register"),
