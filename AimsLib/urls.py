2fb8a9f84a7212dd6f986d0d77e7ce4bd04993dd
aimwps <mwpskinner@gmail.com>
2021-09-28 10:05:37 +0100

    finished deletion, added new includes to urls

--- AimsLib/urls.py
+++ AimsLib/urls.py
@@ -7,15 +7,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    path('', include('forum.urls')),
     path('', include('Development.urls')),
     path('', include('Paths.urls')),
     path('', include('Benchmark.urls')),
     path('', include('VideoLecture.urls')),
     path('', include('WrittenLecture.urls')),
     path('', include('QuestionGenerator.urls')),
-    path('', include("ShareContent.urls")),
+    path('', include("Community.urls")),
+    path('', include("Organisations.urls")),
+    path('', include("WebsiteTools.urls")),
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('Members/', include("django.contrib.auth.urls")),
     path('Members/', include('Members.urls')),
--- forum/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Reply, CommentContent, VoteContent
-# Register your models here.
-admin.site.register(Topic) # Allows posts to be accesible via admin area
-admin.site.register(Reply)
-admin.site.register(CommentContent)
-admin.site.register(VoteContent)
