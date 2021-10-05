c9065547d2710471a3be42ee3ca7c51ff011c05e
aimwps <mwpskinner@gmail.com>
2021-10-05 14:26:53 +0100

    started ajax for quick aims

--- static/js/development/aimsDash.js
+++ static/js/development/aimsDash.js
@@ -16,70 +16,73 @@ function getUncompleteTrackers() {
         var selectDisplay = `#${item.display_section}`;
         console.log(selectDisplay)
         $(selectDisplay).append(`
-          <li class="list-group-item">
-            <p>${item.question}</p>
-              <ul class="nav nav flex-column">
-                <li class="nav-item my-2" id="completeShow_${index}">
+          <li class="list-group-item border border-primary my-2">
+            <p>${item.question}<br><small>submission period: ${ item.period_log_start} to ${item.period_log_end}</small></p>
+
+              <ul class="list-group">
+                <li class="list-group-item border border-white" id="minShow_${index}">
                 </li>
-                <li class="nav-item my-2" id="minShow_${index}">
+                <li class="list-group-item border border-white" id="completeShow_${index}">
                 </li>
-                <li class="nav-item my-2" id="noShow_${index}">
+                <li class="list-group-item border border-white" id="noShow_${index}">
                 </li>
               </ul>
           </li>`);
         if( item.tracker.minimum_show_allowed){
-          if (item.count_quantity == "null"){
+          if (item.count_quantity == null){
           $(`#minShow_${index}`).append(`
-            <div class="row px-0">
-              <div class="col-9">
-                I showed up <small>(${item.tracker.minimum_show_description})</small>
-              </div>
-              <div class="col-3">
-                <a type="button" class="primary"><i class="fas fa-bars"></i></a>
-              </div>
+          <div class="row">
+            <div class="col-3">
             </div>
+            <div class="col-8">
+              <p><strong>Submit, I showed up: </strong> ${item.tracker.minimum_show_description}</p>
+            </div>
+          </div>
             `);
           } else {
             $(`#minShow_${index}`).append(`
-              <div class="row px-0">
-                <div class="col-12">
-                  You've already logged ${item.count_quantity} ${item.tracker.metric_unit} today
-                </div>
+            <div class="row">
+              <div class="col-3">
               </div>
+              <div class="col-9">
+                <p>You've logged ${item.count_quantity} ${item.tracker.metric_unit} already this period. The minimum show option has been disabled.</p>
+              </div>
+            </div>
               `);
           };
         } else {
           $(`#minShow_${index}`).append(`
-            <div class="row px-0">
-              <div class="col-12">
-                Minimum show alternative not allowed.
+          <div class="row">
+            <div class="col-3">
+
+            </div>
+            <div class="col-9">
+              <p>No minimum show option available for this tracker</p>
               </div>
             </div>
             `);
           };
         if(item.tracker.metric_tracker_type =="boolean"){
           $(`#completeShow_${index}`).append(`
-          <div class="row px-0">
-            <div class="col-9">
-              I completed this successfully
-            </div>
+          <div class="row">
             <div class="col-3">
-              <a type="button" class="primary"><i class="fas fa-bars"></i></a>
             </div>
-          </div>`);
+            <div class="col-9">
+              <p>I completed this successfully</p>
+            </div>
+
+            </div>`)
         } else {
           $(`#completeShow_${index}`).append(`
-          <div class="row px-0">
+          <div class="row">
+            <div class="col-3 my-auto">
+              <input type="text" class="form-control" id="inputCountValue_${index}">
+            </div>
             <div class="col-9">
-              <div class="input-group mb-3">
-                <span class="input-group-text" id="countInput">Adding count of: </span>
-                <input type="text" class="form-control" aria-label="Username" aria-describedby="countInput">
-              </div>
+              <p><strong>Submit, count: </strong> Enter an amount of ${item.tracker.metric_unit} you have ${item.tracker.metric_action} this period and submit</p>
             </div>
-            <div class="col-3">
-              <a type="button" class="primary"><i class="fas fa-bars"></i></a>
-            </div>
-          </div>`)
+          </div>
+              `)
         };
       });
       }
