aacdd379a3285b4e92a8cac647bce0686f38df1a
aimwps <mwpskinner@gmail.com>
2022-01-07 14:07:50 +0000

    added code to fix DB

--- WebsiteTools/views.py
+++ WebsiteTools/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from Members.models import MemberProfile
 def home(request):
+    member_profiles = MemberProfile.objects.all()
+    for p in member_profiles:
+        p.delete()
     return render(request, 'home.html', {})
 
 class LoginRegisterRequiredView(TemplateView):
